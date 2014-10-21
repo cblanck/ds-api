37e3559b8da84765f3d3d31c7244a9751539331f
Connor Blanck <connorblanck@gmail.com>
2014-10-21 19:45:46 -0400

    Adding review servlet List

--- servlet_review.go
+++ servlet_review.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 	"net/http"
 	"time"
 )
@@ -22,7 +24,7 @@ type Review struct {
 	Recommend    bool
 }
 
-func NewReviewServlet() *ReviewServlet {
+func NewReviewServlet(server_config Config, session_manager *SessionManager) *ReviewServlet {
 	t := new(ReviewServlet)
 	t.session_manager = session_manager
 
@@ -36,4 +38,35 @@ func NewReviewServlet() *ReviewServlet {
 
 func (t *ReviewServlet) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	HandleServletRequest(t, w, r)
+}
+
+func (t *ReviewServlet) List(w http.ResponseWriter, r *http.Request) {
+	class_id := r.Form.Get("class_id")
+
+	if class_id == "" {
+		ServeError(w, r, "Missing value for one or more fields", 200)
+		return
+	}
+
+	rows, err := t.db.Query("SELECT id, title, recommend FROM review WHERE class_id = ?", class_id)
+	if err != nil {
+		log.Println("List", err)
+		ServeError(w, r, "Internal server error", 500)
+		return
+	}
+
+	defer rows.Close()
+	review_list := make([]*Review, 0)
+	for rows.Next() {
+		review := new(Review)
+		if err := rows.Scan(
+			&review.Id,
+			&review.Title,
+			&review.Recommend); err != nil {
+			log.Println("List", err)
+			ServeError(w, r, "Internal server error", 500)
+		}
+		review_list = append(review_list, review)
+	}
+	ServeResult(w, r, review_list)
 }
