c8e4c1aa34a9e9d0bb514d21eea906e67a8145c2
Connor Blanck <connorblanck@gmail.com>
2014-11-02 22:01:32 -0500

    Starting the single review api

--- servlet_review.go
+++ servlet_review.go
@@ -14,14 +14,16 @@ type ReviewServlet struct {
 }
 
 type Review struct {
-	Id           int
-	User_id      int
-	Date         time.Time
-	Review       string
-	Title        string
-	Professor_id int
-	Class_id     int
-	Recommend    bool
+	Id            int
+	User_id       int
+	Date          time.Time
+	Review        string
+	Title         string
+	Instructor_id int
+	Class_id      int
+	Recommend     bool
+	User          *UserData
+	Instructor    *Instructor
 }
 
 func NewReviewServlet(server_config Config, session_manager *SessionManager) *ReviewServlet {
@@ -69,4 +71,45 @@ func (t *ReviewServlet) List(w http.ResponseWriter, r *http.Request) {
 		review_list = append(review_list, review)
 	}
 	ServeResult(w, r, review_list)
+}
+
+func (t *ReviewServlet) Review(w http.ResponseWriter, r *http.Request) {
+	id := r.Form.Get("Id")
+
+	if id == "" {
+		ServeError(w, r, "Missing value for one or more fields", 200)
+		return
+	}
+
+	rows, err := t.db.Query("SELECT id, user_id, date, review, title, professor_id, class_id, recommend FROM review WHERE id = ?", id)
+
+	if err != nil {
+		log.Println("Review", err)
+		ServeError(w, r, "Internal server error", 500)
+		return
+	}
+
+	defer rows.Close()
+	review := make(Review)
+	for rows.Next() {
+		if err := rows.Scan(
+			&review.Id,
+			&review.User_id,
+			&review.Date,
+			&review.Title,
+			&review.Professor_id,
+			&review.Class_id,
+			&review.Recommend); err != nil {
+			log.Println("Review", err)
+			ServeError(w, r, "Internal server error", 500)
+		}
+	}
+
+	user, err := FetchUserById(t.db, review.User_id)
+	if err != nil {
+		log.Println("Review", err)
+		ServeError(w, r, "Internal server error", 500)
+		return
+	}
+	ServeResult(w, r, review)
 }
